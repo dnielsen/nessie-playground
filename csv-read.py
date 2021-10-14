570bb9e018306da278343a5008edbeba25dfea83
Dave Nielsen <dnielsen@gmail.com>
2021-10-14 01:41:23 -0700

    troubleshooting

--- csv-read.py
+++ csv-read.py
@@ -1,7 +1,19 @@
 """
 Use csv file as a Spark SQL data source.
 Run with:
-  ../spark-3.0.3-bin-hadoop2.7/bin/spark-submit --packages org.apache.iceberg:iceberg-spark3-runtime:0.12.0,org.projectnessie:nessie-spark-extensions:0.8.3 /Users/davenielsen/code/nessie-playground/csv-read.py
+  ../spark-3.0.3-bin-hadoop2.7/bin/spark-submit --packages org.apache.iceberg:iceberg-spark3-runtime:0.11.1,org.projectnessie:nessie-spark-extensions:0.8.3 /Users/davenielsen/code/nessie-playground/csv-read.py
+"""
+import os
+from pyspark.sql import SparkSession
+# from pyspark.sql import Row
+# from pyspark.sql import *
+from pyspark import SparkConf
+import pynessie
+# findspark.init()
+"""
+Use csv file as a Spark SQL data source.
+Run with:
+  ./bin/spark-submit examples/src/main/python/sql/datasource.py
 """
 import os
 from pyspark.sql import SparkSession
@@ -14,7 +26,7 @@ pynessie_version = pynessie.__version__
 
 conf = SparkConf()
 # we need iceberg libraries and the nessie sql extensions
-conf.set("spark.jars.packages", f"org.apache.iceberg:iceberg-spark3-runtime:0.12.0,org.projectnessie:nessie-spark-extensions:{pynessie_version}")
+conf.set("spark.jars.packages", f"org.apache.iceberg:iceberg-spark3-runtime:0.11.1,org.projectnessie:nessie-spark-extensions:{pynessie_version}")
 # ensure python <-> java interactions are w/ pyarrow
 conf.set("spark.sql.execution.pyarrow.enabled", "true")
 # create catalog dev_catalog as an iceberg catalog
@@ -36,11 +48,10 @@ spark = SparkSession.builder.config(conf=conf).getOrCreate()
 df = spark.read.csv("/Users/davenielsen/code/nessie-playground/data/totals_stats.csv")
 df.show()
 
-spark.sql("CREATE BRANCH dev IN nba_catalog AS main").toPandas()
+print("Current working dir : %s" % os.getcwd())
+spark.sql("CREATE BRANCH dev IN dev_catalog AS main")
+print("CREATED BRANCH dev")
 spark.sql("""CREATE TABLE IF NOT EXISTS dev_catalog.warehouse.salaries (Season STRING, Team STRING, Salary STRING, Player STRING) USING iceberg""");
 spark.sql("""CREATE OR REPLACE TEMPORARY VIEW salaries_table USING csv
             OPTIONS (path "/Users/davenielsen/code/nessie-playground/data/salaries.csv", header true)""");
 spark.sql('INSERT INTO dev_catalog.warehouse.salaries SELECT * FROM salaries_table');
-
-
-spark.stop()
